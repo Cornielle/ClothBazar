1c0b163ede86c40d2d5a2183b83fc29c34255380
Cornielle <briancorniellebatista@gmail.com>
2019-03-08 11:23:07 -0400

    With Migrations Done!

--- ClothBazar.Database/CBContext.cs
+++ ClothBazar.Database/CBContext.cs
@@ -5,7 +5,16 @@ namespace ClothBazar.Database
 {
     public class CBContext : DbContext
     {
+        public CBContext()
+            :base("ClothBazarConnection")
+        {
+        }
         public DbSet<Product> Products { get; set; }
         public DbSet<Category> Categories { get; set; }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+        }
     }
 }
--- ClothBazar.Entities/Product.cs
+++ ClothBazar.Entities/Product.cs
@@ -1,13 +1,10 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.ComponentModel.DataAnnotations;
 
 namespace ClothBazar.Entities
 {
     public class Product : BaseEntity
     {
+        [Key]
         public int id { get; set; }
         public decimal Price { get; set; } = 0;
         public Category Category { get; set; }
