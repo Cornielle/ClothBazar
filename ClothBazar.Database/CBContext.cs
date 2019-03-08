32891fd00da996831423a8160007f08817a8f568
Cornielle <briancorniellebatista@gmail.com>
2019-03-08 10:57:37 -0400

    Bootstrap Completely Solved Already 3.4.1 version
    Bootstrap Completely Solved Already 3.4.1 version

---
+++ ClothBazar.Database/CBContext.cs
@@ -0,0 +1,11 @@
+﻿using ClothBazar.Entities;
+using System.Data.Entity;
+
+namespace ClothBazar.Database
+{
+    public class CBContext : DbContext
+    {
+        public DbSet<Product> Products { get; set; }
+        public DbSet<Category> Categories { get; set; }
+    }
+}
